b06dfd26d965174065bb57c8cdb0db8221dee80f
Steven Burris <stevenburris.jr@gmail.com>
2016-09-14 11:33:04 -0400

    Refactor into Player Class

--- src/Game.java
+++ src/Game.java
@@ -4,40 +4,17 @@ import java.util.*;
  * Created by stevenburris on 9/14/16.
  */
 public class Game {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome,traveller");
-        System.out.println("What is your name?");
 
-        Scanner scanner = new Scanner(System.in);
+        Player player = new Player();
+        player.chooseName();
+        player.chooseWeapon();
+        player.chooseLocation();
 
-        String name = scanner.nextLine();
-        System.out.println("Welcome, "+name);
 
-        System.out.println("Do you want a sword or mace?");
-        String weapon = scanner.nextLine();
-
-        if (weapon.equalsIgnoreCase("sword")){
-            System.out.println("Here is your sword!");
-        }
-        else if(weapon.equalsIgnoreCase("mace")){
-            System.out.println("Here is your mace!");
-        }
-        else {
-            throw new Exception("Invalid Weapon");
-        }
-
-        System.out.println("Would you like to go to the tunnel or forest?");
-        String location  =  scanner.nextLine();
-
-        if (location.equalsIgnoreCase("tunnel")){
-            System.out.println("Entering tunnel...");
-        }
-        else if (location.equalsIgnoreCase("forest")){
-            System.out.println("Entering forest...");
-        }
-        else {
-            throw new Exception("Invalid location.");
-        }
     }
 
 
