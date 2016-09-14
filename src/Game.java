a5b972b436dce7bb7f696e8b46fb6bdf17dadbe9
Steven Burris <stevenburris.jr@gmail.com>
2016-09-14 11:12:44 -0400

    Initial Import

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,44 @@
+import java.util.*;
+
 /**
  * Created by stevenburris on 9/14/16.
  */
 public class Game {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Welcome,traveller");
+        System.out.println("What is your name?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        System.out.println("Welcome, "+name);
+
+        System.out.println("Do you want a sword or mace?");
+        String weapon = scanner.nextLine();
+
+        if (weapon.equalsIgnoreCase("sword")){
+            System.out.println("Here is your sword!");
+        }
+        else if(weapon.equalsIgnoreCase("mace")){
+            System.out.println("Here is your mace!");
+        }
+        else {
+            throw new Exception("Invalid Weapon");
+        }
+
+        System.out.println("Would you like to go to the tunnel or forest?");
+        String location  =  scanner.nextLine();
+
+        if (location.equalsIgnoreCase("tunnel")){
+            System.out.println("Entering tunnel...");
+        }
+        else if (location.equalsIgnoreCase("forest")){
+            System.out.println("Entering forest...");
+        }
+        else {
+            throw new Exception("Invalid location.");
+        }
     }
+
+
 }
